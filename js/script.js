237fed552a1c9d5ee3d4741cf3d7cd3dbcb90a3f
cxgzsgf <68316980+cxgzsgf@users.noreply.github.com>
2020-07-18 09:49:20 +0800

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 
-swal("开始这个完美的弹出框旅程吧！")
+
+var btn = document.getElementById('open_btn');
+var div = document.getElementById('background');
+var close = document.getElementById('close-button');
+ 
+btn.onclick = function show() {
+	div.style.display = "block";
+}
+ 
+close.onclick = function close() {
+	div.style.display = "none";
+}
+ 
+window.onclick = function close(e) {
+	if (e.target == div) {
+		div.style.display = "none";
+	}
+}

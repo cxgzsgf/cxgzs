32a9ba6c13e8f1e82b5848d3361b81bdbb7f58c4
cxgzsgf <68316980+cxgzsgf@users.noreply.github.com>
2020-07-18 22:38:30 +0800

    Batch update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ text: "Nice to meet you!",
 type: "success",
 confirmButtonText: "HaHa"
 })
+function firm() { 
+        //利用对话框返回的值 （true 或者 false） 
+        if (confirm("是否前往我们的禁言倒计时器？")) { 
+            <script language="javascript" type="text/javascript">
+window.location.href="https://www.timetaco.com/wzhrzv/";

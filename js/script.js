9ffd3aba78ca76a9ca033d7471748f3e41a58b69
cxgzsgf <68316980+cxgzsgf@users.noreply.github.com>
2020-07-20 14:09:37 +0800

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,25 +1,2 @@
 
-swal({
-title: "OK!",
-text: "Nice to meet you!",
-type: "success",
-confirmButtonText: "HaHa"
-})
-Splitting()
-gsap.set('.char', {
-  '--hue': gsap.utils.distribute({
-    base: 0,
-    amount: 330,
-  })
-})
-gsap.to('.char', {
-  '--weight': 700,
-  '--saturation': 80,
-  ease: 'sine.inOut',
-  duration: 0.5,
-  stagger: {
-    yoyo: true,
-    repeat: -1,
-    each: 0.15,
-  }
-}).time(2)
+

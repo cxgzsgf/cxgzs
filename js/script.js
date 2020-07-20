a264bdf5e041fa0aafd73d644da43d1dd261b954
cxgzsgf <68316980+cxgzsgf@users.noreply.github.com>
2020-07-20 21:04:29 +0800

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 
-
+<script>
+function openly(){
+        if(window.name==""){
+            window.location.href="hy.html";
+        }
+        window.name="onload";
+}
+setTimeout('openly()',5000);

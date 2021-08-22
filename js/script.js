c41ec2106c052ae31091300e684c503ef4fc080e
cxgzsgf <68316980+cxgzsgf@users.noreply.github.com>
2021-08-22 23:03:21 +0800

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 
-<script>
-function openly(){
-        if(window.name==""){
-            window.location.href="call.html";
-        }
-        window.name="onload";
-}
-setTimeout('openly()',5000);
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
